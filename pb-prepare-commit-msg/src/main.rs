cd92253ce5281dcd610e83a25eb303d6b3d0aad9
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2020-04-22 15:38:31 +0200

    Update main.rs

--- pb-prepare-commit-msg/src/main.rs
+++ pb-prepare-commit-msg/src/main.rs
@@ -17,7 +17,7 @@ fn main() {
         )
         .arg(
             Arg::with_name("commit-message-source")
-                .help("The second is the source of the commit message, and can be: message (if a -m or -F option was given to git); template (if a -t option was given or the configuration option commit.template is set in git); merge (if the commit is a merge or a .git/MERGE_MSG file exists); squash (if a .git/SQUASH_MSG file exists); or commit")
+                .help("The commit message, and can be: message (if a -m or -F option was given to git); template (if a -t option was given or the configuration option commit.template is set in git); merge (if the commit is a merge or a .git/MERGE_MSG file exists); squash (if a .git/SQUASH_MSG file exists); or commit")
                 .index(2)
                 .required(false)
         )
