a98cb2946eb876aa40129ffb8d95ae3311eb47eb
Billie Thompson <billie@billiecodes.com>
2020-04-26 00:01:51 +0200

    Added more outputs to the longer form of the test

--- pb-pre-commit/tests/e2e_able_to_list_version.rs
+++ pb-pre-commit/tests/e2e_able_to_list_version.rs
@@ -61,36 +61,42 @@ fn version_returned_by_short_flag() {
         .output()
         .expect("failed to execute process");
 
-    let stderr = str::from_utf8(&output.stderr)
-        .expect("Failed to convert stdout to a string, is it valid UTF-8?");
     let stdout = str::from_utf8(&output.stdout)
+        .expect("Failed to convert stdout to a string, is it valid UTF-8?");
+    let stderr = str::from_utf8(&output.stderr)
         .expect("Failed to convert stderr to a string, is it valid UTF-8?");
 
-    assert!(
-        stderr.is_empty(),
-        "Expected stderr to be empty, instead got stdout: {:?} stderr: {:?}",
-        stdout,
-        stderr
-    );
-
     let expected_prefix = "pb-pre-commit ";
+    let status = output.status;
+
     assert!(
         stdout.starts_with(expected_prefix),
-        "Expected stdout to start with {:?}, instead got stdout: {:?} stderr: {:?}",
+        "Expected stdout to start with {:?}, instead got status: {:?} stdout: {:?} stderr: {:?}",
         expected_prefix,
+        status,
         stdout,
         stderr
     );
     assert!(
         stdout.ends_with("\n"),
-        "Expected stdout to end with a new line, instead got stdout: {:?} stderr: {:?}",
+        "Expected stdout to end with a new line, instead got status: {:?} stdout: {:?} stderr: {:?}",
+        status,
         stdout,
         stderr
     );
 
     assert!(
-        &output.status.success(),
-        "Expected command to run successfully, instead got {:?}",
-        output.status.code()
+        stderr.is_empty(),
+        "Expected stderr to be empty, instead got status: {:?} stdout: {:?} stderr: {:?}",
+        status,
+        stdout,
+        stderr
+    );
+    assert!(
+        &status.success(),
+        "Expected command to run successfully, instead got status: {:?} stdout: {:?} stderr: {:?}",
+        status,
+        stdout,
+        stderr
     );
 }
