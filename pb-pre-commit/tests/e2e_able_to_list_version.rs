51b4faa193e3f62990ade9f0ba0b0559a7a99380
Billie Thompson <billie@billiecodes.com>
2020-04-25 23:56:33 +0200

    Added status to list
    This adds status to the things that we output on error

--- pb-pre-commit/tests/e2e_able_to_list_version.rs
+++ pb-pre-commit/tests/e2e_able_to_list_version.rs
@@ -17,30 +17,37 @@ fn version_returned_by_long_flag() {
         .expect("Failed to convert stderr to a string, is it valid UTF-8?");
 
     let expected_prefix = "pb-pre-commit ";
+    let status = output.status;
+
     assert!(
         stdout.starts_with(expected_prefix),
-        "Expected stdout to start with {:?}, instead got stdout: {:?} stderr: {:?}",
+        "Expected stdout to start with {:?}, instead got status: {:?} stdout: {:?} stderr: {:?}",
         expected_prefix,
+        status,
         stdout,
         stderr
     );
     assert!(
         stdout.ends_with("\n"),
-        "Expected stdout to end with a new line, instead got stdout: {:?} stderr: {:?}",
+        "Expected stdout to end with a new line, instead got status: {:?} stdout: {:?} stderr: {:?}",
+        status,
         stdout,
         stderr
     );
 
     assert!(
         stderr.is_empty(),
-        "Expected stderr to be empty, instead got stdout: {:?} stderr: {:?}",
+        "Expected stderr to be empty, instead got status: {:?} stdout: {:?} stderr: {:?}",
+        status,
         stdout,
         stderr
     );
     assert!(
-        &output.status.success(),
-        "Expected command to run successfully, instead got {:?}",
-        output.status.code()
+        &status.success(),
+        "Expected command to run successfully, instead got status: {:?} stdout: {:?} stderr: {:?}",
+        status,
+        stdout,
+        stderr
     );
 }
 
