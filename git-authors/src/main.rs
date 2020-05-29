8995094db131b60a0ccb6013edef4ea3a8cd0ccc
Billie Thompson <billie@billiecodes.com>
2020-05-30 00:49:53 +0200

    Only unwrap in main
    This changes one of our exes to make sure we're only unwrapping in main.

--- git-authors/src/main.rs
+++ git-authors/src/main.rs
@@ -34,7 +34,7 @@ const TIMEOUT: &str = "timeout";
 
 fn main() {
     let cargo_package_name = env!("CARGO_PKG_NAME");
-    let default_config_file = config_file_path(cargo_package_name);
+    let default_config_file = config_file_path(cargo_package_name).unwrap();
 
     let matches = App::new(cargo_package_name)
         .version(crate_version!())
@@ -76,8 +76,8 @@ fn main() {
         )
         .get_matches();
 
-    let author_config = get_author_config(&matches);
-    let authors_initials = get_author_initials(&matches);
+    let author_config = get_author_config(&matches).unwrap();
+    let authors_initials = get_author_initials(&matches).unwrap();
     let yaml_authors: Authors = get_authors_from_user_config(&author_config).unwrap();
     let selected_authors: Vec<Option<&Author>> = yaml_authors.get(&authors_initials);
     let failed_authors: Vec<_> = selected_authors
@@ -102,76 +102,70 @@ You can fix this by checking the initials are in the configuration file.
         std::process::exit(InitialNotMatchedToAuthor as i32);
     }
 
-    let current_dir = env::current_dir().expect("Unable to retrieve current directory");
-    let get_repository_config = |x: Repository| x.config();
-    let get_default_config = |_| Config::open_default();
+    let current_dir = env::current_dir().unwrap();
     let mut git_config = Repository::discover(current_dir)
-        .and_then(get_repository_config)
-        .or_else(get_default_config)
+        .and_then(|x: Repository| x.config())
+        .or_else(|_| Config::open_default())
         .map(Git2::new)
-        .expect("Couldn't load any git config");
+        .unwrap();
 
     let authors = selected_authors.into_iter().flatten().collect::<Vec<_>>();
     set_authors(
         &mut git_config,
         &authors,
-        Duration::from_secs(get_timeout(&matches) * 60),
+        Duration::from_secs(get_timeout(&matches).unwrap() * 60),
     )
-    .expect("Couldn't set author")
+    .unwrap()
 }
 
-fn get_author_initials<'a>(matches: &'a ArgMatches) -> Vec<&'a str> {
-    matches.values_of(AUTHOR_INITIAL).unwrap().collect()
+fn get_author_initials<'a>(matches: &'a ArgMatches) -> Option<Vec<&'a str>> {
+    matches.values_of(AUTHOR_INITIAL).map(Iterator::collect)
 }
 
-fn get_author_config(matches: &ArgMatches) -> String {
+fn get_author_config(matches: &ArgMatches) -> Result<String, Box<dyn Error>> {
     match matches.value_of(AUTHOR_FILE_COMMAND) {
         Some(command) => get_author_config_from_exec(command),
         None => get_author_config_from_file(matches),
     }
 }
 
-fn get_author_config_from_exec(command: &str) -> String {
-    String::from_utf8(
-        Command::new(env::var("SHELL").unwrap_or_else(|_| String::from("sh")))
-            .stderr(Stdio::inherit())
-            .arg("-c")
-            .arg(command)
-            .output()
-            .unwrap()
-            .stdout,
-    )
-    .unwrap()
+fn get_author_config_from_exec(command: &str) -> Result<String, Box<dyn Error>> {
+    Command::new(env::var("SHELL").unwrap_or_else(|_| String::from("sh")))
+        .stderr(Stdio::inherit())
+        .arg("-c")
+        .arg(command)
+        .output()
+        .map_err(Box::<dyn Error>::from)
+        .and_then(|x| String::from_utf8(x.stdout).map_err(Box::from))
 }
 
-fn get_author_config_from_file(matches: &ArgMatches) -> String {
-    fs::read_to_string(get_author_file_path(&matches))
-        .expect("Something went wrong reading the file")
+fn get_author_config_from_file(matches: &ArgMatches) -> Result<String, Box<dyn Error>> {
+    get_author_file_path(&matches)
+        .ok_or_else(|| -> Box<dyn Error> { "Expected a author file path, didn't find one".into() })
+        .map_err(Box::from)
+        .and_then(|x| fs::read_to_string(x).map_err(Box::from))
 }
 
-fn get_author_file_path<'a>(matches: &'a ArgMatches) -> &'a str {
-    matches.value_of(AUTHOR_FILE_PATH).unwrap()
+fn get_author_file_path<'a>(matches: &'a ArgMatches) -> Option<&'a str> {
+    matches.value_of(AUTHOR_FILE_PATH)
 }
 
-fn get_timeout(matches: &ArgMatches) -> u64 {
+fn get_timeout(matches: &ArgMatches) -> Result<u64, Box<dyn Error>> {
     matches
         .value_of(TIMEOUT)
         .ok_or_else(|| -> Box<dyn Error> { "No timeout set".into() })
-        .and_then(|x| -> Result<u64, Box<dyn Error>> { x.parse::<u64>().map_err(Box::from) })
-        .unwrap()
+        .and_then(|x| x.parse().map_err(Box::from))
 }
 
-fn config_file_path(cargo_package_name: &str) -> String {
+fn config_file_path(cargo_package_name: &str) -> Result<String, Box<dyn Error>> {
     xdg::BaseDirectories::with_prefix(cargo_package_name.to_string())
         .map_err(Box::<dyn std::error::Error>::from)
         .and_then(|x| authors_config_file(&x))
-        .unwrap()
-        .to_string_lossy()
-        .to_string()
+        .map(|x| x.to_string_lossy().into())
 }
 
 fn authors_config_file(config_directory: &BaseDirectories) -> Result<PathBuf, Box<dyn Error>> {
     config_directory
         .place_config_file("authors.yml")
-        .map_err(Box::from)
+        .map_err(|x| x.into())
 }
