7de59399fd174ccd478b3ea3325ed845aed547c8
Billie Thompson <billie@billiecodes.com>
2020-06-20 07:33:02 +0200

    Use generate to
    This lets us delete a bunch of lines from our build rs

--- git-mit-config/build.rs
+++ git-mit-config/build.rs
@@ -1,5 +1,5 @@
 use clap_generate::generators::{Bash, Elvish, Fish};
-use clap_generate::{generate, Generator};
+use clap_generate::{generate_to, Generator};
 use std::{env, fs};
 
 use mit_commit_message_lints::lints::Lint;
@@ -9,32 +9,19 @@ use std::path::PathBuf;
 mod cli;
 
 fn main() {
-    let cargo_package_name = env!("CARGO_PKG_NAME");
     let lint_names: Vec<_> = Lint::iterator()
         .map(mit_commit_message_lints::lints::Lint::name)
         .collect();
     let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
 
-    generate_completion::<Elvish>(
-        &lint_names,
-        format!("{}.elv", cargo_package_name),
-        &out_dir.join("elvish_completion"),
-    );
-    generate_completion::<Fish>(
-        &lint_names,
-        format!("{}.fish", cargo_package_name),
-        &out_dir.join("fish_completion"),
-    );
+    generate_completion::<Elvish>(&lint_names, &out_dir.join("elvish_completion"));
+    generate_completion::<Fish>(&lint_names, &out_dir.join("fish_completion"));
     // This segfaults at the moment
-    //generate_completion::<Zsh>(&lint_names, format!("_{}", cargo_package_name), &out_dir.join("zsh_completion"));
-    generate_completion::<Bash>(
-        &lint_names,
-        cargo_package_name.into(),
-        &out_dir.join("zsh_completion"),
-    );
+    // generate_completion::<Zsh>(&lint_names, &out_dir.join("zsh_completion"));
+    generate_completion::<Bash>(&lint_names, &out_dir.join("bash_completion"));
 }
 
-fn generate_completion<T>(lint_names: &[&str], filename: String, dir: &PathBuf)
+fn generate_completion<T>(lint_names: &[&str], dir: &PathBuf)
 where
     T: Generator,
 {
@@ -43,7 +30,5 @@ where
     }
 
     fs::create_dir(dir.clone()).unwrap();
-    let file_path = dir.join(filename);
-    let mut file = fs::File::create(file_path).unwrap();
-    generate::<T, _>(&mut cli::app(lint_names), env!("CARGO_PKG_NAME"), &mut file);
+    generate_to::<T, _, _>(&mut cli::app(lint_names), env!("CARGO_PKG_NAME"), &dir);
 }
