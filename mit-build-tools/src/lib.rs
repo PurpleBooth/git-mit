f97d06a2005ea27c400849256dc27a3134eff55e
Billie Thompson <billie@billiecodes.com>
2020-06-20 14:05:28 +0200

    Escape the markdown
    We can escape things in pandoc markdown like github markdown.

---
+++ mit-build-tools/src/lib.rs
@@ -0,0 +1,2 @@
+pub mod completion;
+pub mod manpage;
---
+++ mit-build-tools/src/manpage/formatters.rs
@@ -0,0 +1,40 @@
+use tinytemplate::error::Error;
+
+use tinytemplate::format_unescaped;
+
+/// Format a string as uppercase
+///
+/// # Errors
+///
+/// Errors if the value isn't string or null
+pub fn format_upper(value: &serde_json::Value, output: &mut String) -> Result<(), Error> {
+    let mut string_value = String::new();
+    format_escape(value, &mut string_value)?;
+
+    output.push_str(&string_value.to_uppercase());
+
+    Ok(())
+}
+
+/// Escape special markdown sequences
+///
+/// # Errors
+///
+/// Errors if the value isn't string or null
+pub fn format_escape(value: &serde_json::Value, output: &mut String) -> Result<(), Error> {
+    let mut string_value = String::new();
+    format_unescaped(value, &mut string_value)?;
+
+    output.push_str(
+        &string_value
+            .replace("[", "\\[")
+            .replace("]", "\\]")
+            .replace("(", "\\(")
+            .replace(")", "\\)")
+            .replace("`", "\\`")
+            .replace("<", "\\<")
+            .replace(">", "\\>"),
+    );
+
+    Ok(())
+}
