57a21379c7a49d68bec82c2e5ffef1e933911b0e
Billie Thompson <billie@billiecodes.com>
2020-07-01 09:27:20 +0200

    Remove old commit and trailer libraries

--- mit-commit-message-lints/src/lints/lib/mod.rs
+++ mit-commit-message-lints/src/lints/lib/mod.rs
@@ -1,21 +1,16 @@
-pub use commit_message::CommitMessage;
-pub use commit_message::Error as CommitMessageError;
 pub use error::Error;
 pub use lint::Error as LintError;
 pub use lint::Lint;
 pub use lints::Error as LintsError;
 pub use lints::Lints;
 pub use problem::{Code, Problem};
-pub use trailer::Trailer;
 
-mod commit_message;
 mod error;
 mod lint;
 mod lints;
 mod subject_longer_than_72_characters;
 mod subject_not_capitalized;
 mod subject_not_seperate_from_body;
-mod trailer;
 
 mod duplicate_trailers;
 mod missing_github_id;
--- mit-commit-message-lints/src/lints/mod.rs
+++ mit-commit-message-lints/src/lints/mod.rs
@@ -2,8 +2,6 @@ pub use cmd::lint;
 pub use cmd::set_status;
 pub use cmd::SetStatusError;
 pub use lib::Code;
-pub use lib::CommitMessage;
-pub use lib::CommitMessageError;
 pub use lib::Lint;
 pub use lib::LintError;
 pub use lib::Lints;
