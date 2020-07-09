c1a1eabcad0a290de2142708cc3c0a9c71ca409d
Billie Thompson <billie@billiecodes.com>
2020-07-09 23:34:58 +0200

    fix: Better error messages from hook installation
    Relates-to: #219

--- git-mit-install/src/main.rs
+++ git-mit-install/src/main.rs
@@ -1,5 +1,6 @@
 use std::{env, fs, io};
 
+use std::path::PathBuf;
 use thiserror::Error;
 
 mod cli;
@@ -15,20 +16,41 @@ fn main() -> Result<(), GitMitInstallError> {
         fs::create_dir(&hooks)?;
     }
 
-    let mit_prepare_commit_msg = which::which("mit-prepare-commit-msg").unwrap();
-    std::os::unix::fs::symlink(mit_prepare_commit_msg, &hooks.join("prepare-commit-msg"))?;
+    install_hook(&hooks, "prepare-commit-msg")?;
+    install_hook(&hooks, "pre-commit")?;
+    install_hook(&hooks, "commit-msg")?;
+    Ok(())
+}
 
-    let mit_prepare_commit_msg = which::which("mit-pre-commit").unwrap();
-    std::os::unix::fs::symlink(mit_prepare_commit_msg, &hooks.join("pre-commit"))?;
+fn install_hook(hook_path: &PathBuf, hook_name: &str) -> Result<(), GitMitInstallError> {
+    let binary_path = which::which(format!("mit-{}", hook_name)).unwrap();
+    let install_path = hook_path.join(hook_name);
+    let install_path_destination = install_path.read_link();
+    if let Ok(existing_hook_path) = install_path_destination.and_then(|x| x.canonicalize()) {
+        if existing_hook_path == install_path {
+            return Ok(());
+        }
+    }
 
-    let mit_prepare_commit_msg = which::which("mit-commit-msg").unwrap();
-    std::os::unix::fs::symlink(mit_prepare_commit_msg, &hooks.join("commit-msg"))?;
+    if install_path.exists() {
+        eprintln!("Couldn't create hook at {}, it already exists, you need to remove this before continuing", install_path.to_string_lossy());
+
+        if let Ok(dest) = install_path.read_link() {
+            eprintln!("looks like it's a symlink to {}", dest.to_string_lossy());
+        }
+
+        return Err(GitMitInstallError::ExistingHook);
+    }
+
+    std::os::unix::fs::symlink(binary_path, &install_path)?;
 
     Ok(())
 }
 
 #[derive(Error, Debug)]
 pub enum GitMitInstallError {
+    #[error("failed install hook")]
+    ExistingHook,
     #[error("failed to find git repository: {0}")]
     Git2(#[from] git2::Error),
     #[error("failed to install hooks: {0}")]
