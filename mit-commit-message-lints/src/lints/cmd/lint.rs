70a3818b58d57c88a146a3d55190f786a8e41d3e
Billie Thompson <billie@billiecodes.com>
2020-08-09 21:16:30 +0200

    feat: Run the lints in parallel

--- mit-commit-message-lints/src/lints/cmd/lint.rs
+++ mit-commit-message-lints/src/lints/cmd/lint.rs
@@ -1,11 +1,14 @@
 use mit_commit::CommitMessage;
 
 use crate::lints::lib::{Lints, Problem};
+use rayon::prelude::*;
 
 #[must_use]
 pub fn lint(commit_message: &CommitMessage, lints: Lints) -> Vec<Problem> {
     lints
         .into_iter()
+        .collect::<Vec<_>>()
+        .into_par_iter()
         .flat_map(|lint| lint.lint(commit_message))
         .collect::<Vec<Problem>>()
 }
