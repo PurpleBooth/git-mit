5f2d2ad2c1aabb558f66af2af4b532266c445f50
Billie Thompson <billie.thompson@armakuni.com>
2023-08-30 16:32:45 +0200

    fix: Remove unneeded mutability

--- mit-commit-message-lints/src/relates/cmd/get_relate_to_configuration_test.rs
+++ mit-commit-message-lints/src/relates/cmd/get_relate_to_configuration_test.rs
@@ -15,9 +15,9 @@ fn there_is_no_relate_config_if_it_has_expired() {
     let now_minus_10 = epoch_with_offset(subtract_10_seconds);
     let mut strings: BTreeMap<String, String> = BTreeMap::new();
     strings.insert("mit.relate.expires".into(), format!("{now_minus_10}"));
-    let mut vcs = InMemory::new(&mut strings);
+    let vcs = InMemory::new(&mut strings);
 
-    let actual = get_relate_to_configuration(&mut vcs).expect("Failed to read VCS config");
+    let actual = get_relate_to_configuration(&vcs).expect("Failed to read VCS config");
     let expected = None;
     assert_eq!(
         expected, actual,
@@ -33,9 +33,9 @@ fn we_get_relate_to_config_back_if_there_is_any() {
         format!("{}", epoch_with_offset(add_10_seconds)),
     );
     buffer.insert("mit.relate.to".into(), "[#12345678]".into());
-    let mut vcs = InMemory::new(&mut buffer);
+    let vcs = InMemory::new(&mut buffer);
 
-    let actual = get_relate_to_configuration(&mut vcs).expect("Failed to read VCS config");
+    let actual = get_relate_to_configuration(&vcs).expect("Failed to read VCS config");
     let expected = Some(RelateTo::from("[#12345678]"));
 
     assert_eq!(
