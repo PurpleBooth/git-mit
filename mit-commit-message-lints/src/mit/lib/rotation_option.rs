2b82dcff3b7960a71a3db33b59017654abe73c41
Billie Thompson <purplebooth@noreply.codeberg.org>
2026-06-15 18:02:46 +0200

    feat: add Random variant to RotationOption

--- mit-commit-message-lints/src/mit/lib/rotation_option.rs
+++ mit-commit-message-lints/src/mit/lib/rotation_option.rs
@@ -14,9 +14,12 @@ use crate::mit::lib::errors::DeserializeRotationOptionError;
 pub enum RotationOption {
     /// Rotate through authors in order, one per commit
     RoundRobin,
+    /// Shuffle authors randomly on each commit
+    Random,
 }
 
 const ROUND_ROBIN_DISPLAY: &str = "round-robin";
+const RANDOM_DISPLAY: &str = "random";
 
 impl FromStr for RotationOption {
     type Err = DeserializeRotationOptionError;
@@ -24,6 +27,7 @@ impl FromStr for RotationOption {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.to_ascii_lowercase().as_str() {
             ROUND_ROBIN_DISPLAY => Ok(Self::RoundRobin),
+            RANDOM_DISPLAY => Ok(Self::Random),
             _ => Err(DeserializeRotationOptionError { src: s.into() }),
         }
     }
@@ -33,6 +37,7 @@ impl Display for RotationOption {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::RoundRobin => write!(f, "{ROUND_ROBIN_DISPLAY}"),
+            Self::Random => write!(f, "{RANDOM_DISPLAY}"),
         }
     }
 }
